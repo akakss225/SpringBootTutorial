7425067161f592c522bec83c38d87a01bf9e7c2b
akakss225 <akakss225@naver.com>
2022-04-04 18:30:11 +0900

    MemberRepository + Test 해보기

---
+++ src/main/java/com/example/hellospring/domain/Member.java
@@ -0,0 +1,11 @@
+package com.example.hellospring.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long id;
+    private String name;
+
+}
---
+++ src/test/java/com/example/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.example.hellospring.repository;
+
+import com.example.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+
+    // 기본적으로 class level의 단위테스트를 돌리면, method 입력 순서와 별개로 무작위로 실행된다.
+    // 따라서 각 method 실행 이후, data를 clear 해줄 필요가 있다 >> 다른 메소드에 영향을 미침
+    @AfterEach // 각 메소드가 끝날때 마다 동작을 수행시켜주는 어노테이션 >> 콜백 어노테이션
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("Spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("Spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("Spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("Spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public  void findAll(){
+        Member member1 = new Member();
+        member1.setName("Spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("Spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+
+}
