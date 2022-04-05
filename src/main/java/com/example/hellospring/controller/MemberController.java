6c420568316e8b8571eecea10326fec2db9af7c0
akakss225 <akakss225@naver.com>
2022-04-05 17:09:09 +0900

    회원등록.html + MemeberController

--- src/main/java/com/example/hellospring/controller/MemberController.java
+++ src/main/java/com/example/hellospring/controller/MemberController.java
@@ -1,16 +1,36 @@
 package com.example.hellospring.controller;
 
+import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 스프링이 실행될 때 이 어노테이션이 있다면, 스프링 컨테이너에 bean 객체로 넣어두고 관리함.
 public class MemberController {
 
     // DI
-    private final MemberService memberService;
+    private MemberService memberService;
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+
     }
 }
